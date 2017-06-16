f351ec7b9f342762db5067cc87a4570211e8fa74
Rome Dorado <romedorado@gmail.com>
2017-06-16 14:54:51 +0800

    first

--- config/index.js
+++ config/index.js
@@ -6,8 +6,7 @@ if(process.env.NODE_ENV === 'production') {
 			PAGE_ACCESS_TOKEN: process.env.PAGE_ACCESS_TOKEN,
 			VERIFY_TOKEN: process.env.VERIFY_TOKEN,
 			APP_SECRET: process.env.APP_SECRET
-		},
-		MONGO_URI: process.env.MONGOURI
+		}
 	}
 } else {
 	module.exports = require('./development.json');
